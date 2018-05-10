e3b6e543ae5d858f542dda10ea43f0353151686e
Michael Bach <migobach@gmail.com>
2018-05-10 16:20:53 -0600

    assignment names

--- main.rb
+++ main.rb
@@ -1,16 +1,16 @@
 # Basic Objectives:
 
-# - User inputs question
-# - Computer outputs random answer
-# - User inputs "QUIT"
-# - Computer outputs a goodbye message and exits
-# - ability to add more answers:
-# - via easter egg question ("add_answers")
-# - do not let them add the same answer if the eight ball already has that answer
+# - User inputs question - Amber
+# - Computer outputs random answer - Michael
+# - User inputs "QUIT" - Bianca
+# - Computer outputs a goodbye message and exits - Bianca
+# - ability to add more answers: - Bianca - maybe :)
+# - via easter egg question ("add_answers") - Come back
+# - do not let them add the same answer if the eight ball already has that answer - Michael
 # - ability to reset answers back to the original bank (hint: think arr.clone)
 # - via easter egg question ("reset_answers")
-# - ability to have eight ball print all answers
-# - via easter egg question ("print_answers")
+# - ability to have eight ball print all answers - Robin
+# - via easter egg question ("print_answers") - 
 # Bonus Objectives:
 
 # -ability to use script arguments when the magic eight ball is started to do bonus functionality above
