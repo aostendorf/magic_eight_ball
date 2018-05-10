74af95c0872d6ebb0caf9198c4f2cc03655410d5
Amber Ostendorf <ambersed_2000@yahoo.com>
2018-05-10 15:11:08 -0600

    init commit

---
+++ main.rb
@@ -0,0 +1,18 @@
+# Basic Objectives:
+
+# - User inputs question
+# - Computer outputs random answer
+# - User inputs "QUIT"
+# - Computer outputs a goodbye message and exits
+# - ability to add more answers:
+# - via easter egg question ("add_answers")
+# - do not let them add the same answer if the eight ball already has that answer
+# - ability to reset answers back to the original bank (hint: think arr.clone)
+# - via easter egg question ("reset_answers")
+# - ability to have eight ball print all answers
+# - via easter egg question ("print_answers")
+# Bonus Objectives:
+
+# -ability to use script arguments when the magic eight ball is started to do bonus functionality above
+# - for example ruby magic_eight.RB add_answers
+# this would start your script but instead of running the regular way you would be prompted to add answers first
