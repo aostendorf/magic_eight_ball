cf76e4fd3e282c1ccb72e5ff0c3410e0f62e1da3
Michael Bach <migobach@gmail.com>
2018-05-10 20:43:25 -0600

    refactored 3 option

--- main.rb
+++ main.rb
@@ -2,32 +2,11 @@ require 'colorize'
 require 'pry'
 
 class Roll
-  # attr_accessor :roll, :roll_array, :add_answer
+  attr_accessor :roll, :roll_array
 
   def initialize
     roll_array
     instructions
-  end
-
-  def instructions
-    puts "1. Welcome to the magic eight ball!"
-    puts "2. Roll the magic ball to see your future"
-    puts "3. Don't like your answer? Choose your own"
-    puts "4. Would you like a list of your answers?"
-    puts "3. Quit"
-    menu
-  end
-
-
-  def questions
-  end
-
-
-  def answers
-    puts "Hit the Enter button to see your future!".colorize(:red)
-    Roll.new = magic_answer
-    magic_answer = @answers.roll_array
-    puts "Your answer #{@input} "
   end
 
   def roll_array
@@ -53,24 +32,63 @@ class Roll
       "Very doubtful".colorize(:red),
       "Hell nah".colorize(:red),
     ]
-    # puts @roll.sample
+    instructions
+  end
+end
+
+  def instructions
+    puts ""
+    puts "*" * 60
+    puts "Welcome to the magic eight ball!"
+    puts "\t1. Roll the magic ball to see your future"
+    puts "\t2. Would you like a list of your answers?"
+    puts "\t3. Quit"
+    puts "*" * 60
+    menu
+  end
+
+  def answers
+    puts "Enter you question below and hit ENTER for an answer.".colorize(:red)
+    puts "> "
+    user_question = gets.strip
+    puts "Your answer: #{@roll.sample}"
+  end
+
+  def print_answers
+    puts "Here is a list of the answers:"
+    puts "#{@answers}"
+    instructions
+  end
+
+  def easter_egg
+    puts "You found the secret portal!".colorize(:pink)
+    puts "Add your own answer, those ones were lame anyway.".colorize(:yellow)
+    puts "> "
+    user_input = gets.strip
+    @answers << user_input
+    instructions
+    puts
   end
 
   def menu
     selection = gets.strip.to_i
     case selection
-    when 2
+    when 1
       answers
+    when 2
+      print_answers
+    when 3
+      puts "Goodbye"
+      exit
+    when 4
+      easter_egg
     else
-      puts "Invalid entry"
+      puts "Invalid entry, try again"
+      instructions
     end
   end
 
-  def add_answer(input)
-    @input = input
-    @roll << @input
-    puts "Your answer: #{@input}"
-  end
-end
 
 Roll.new
+
+# :add_answer :new_answers :easter_egg
